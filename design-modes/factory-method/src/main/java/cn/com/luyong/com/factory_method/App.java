7dd9dcf91429be5d3d6e56fe431d7d0e19545377
luyong <ly@bsfit.com.cn>
2015-10-28 17:11:19 +0800

    普通工程模式

---
+++ design-modes/factory-method/src/main/java/cn/com/luyong/com/factory_method/App.java
@@ -0,0 +1,13 @@
+package cn.com.luyong.com.factory_method;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
